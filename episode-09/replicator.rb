a2d78ac450bb0f065dd96759a93f9342fd92e2ae
DocPrior <Sara Prior>
2017-05-29 14:58:11 -0400

    finish 9

--- episode-09/replicator.rb
+++ episode-09/replicator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Replicator
 
   attr_reader :plate
@@ -33,7 +35,6 @@ class Replicator
 
   def transport_ingredients_to_glass
     return unless glass_in_tummy
-
     @recipe.ingredients.each do |ingredient_name|
       @enterprise.transporter.energize(obj: @enterprise.pantry.find_ingredient(ingredient_name), from: @enterprise.pantry.shelf, to: glass_in_tummy.inside)
     end
@@ -41,7 +42,6 @@ class Replicator
 
   def mix
     return unless glass_in_tummy
-
     if @power && @enterprise.reactor.draw_power(3)
       glass_in_tummy.inside.contents.shuffle!.compact!
     end
